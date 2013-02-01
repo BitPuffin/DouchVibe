3724e69e17d05382d9a0555356eaff6e0bb82fce
Isak Andersson <BitPuffin@lavabit.com>
2013-02-01 10:00:32 +0100

    Clear out db, this file will host all of the library

--- source/douchvibe/db.d
+++ source/douchvibe/db.d
@@ -1,12 +1,2 @@
-module douchvibe.client;
+module douchvibe.db;
 
-class CouchClient {
-    public:
-
-        this(string host = "localhost", int port = 5984, string user = null, string password = null, bool is_https = false) {
-
-        }
-
-        ~this() {
-        }
-}
