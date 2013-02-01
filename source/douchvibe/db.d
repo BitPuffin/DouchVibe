386f941d4a475b7b0fec926ec288cf110896d8aa
Isak Andersson <BitPuffin@lavabit.com>
2013-02-01 10:55:57 +0100

    Add some basic types and start working on REST interface

--- source/douchvibe/db.d
+++ source/douchvibe/db.d
@@ -1,2 +1,26 @@
 module douchvibe.db;
 
+import vibe.http.client;
+import vibe.data.json;
+
+class Server {
+    
+}
+
+class Database {
+
+}
+
+class RestClient {
+    private:
+        HttpClient client;
+
+    public:
+        this(string host="127.0.0.1", ushort port = 5984u, bool ssl=false) {
+            client = connectHttp(host, port, ssl);
+        }
+
+        ~this() {
+            client.disconnect();
+        }
+}
