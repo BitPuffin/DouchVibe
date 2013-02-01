6bf555e806cd4f9cc769c9d8dfa620d754e172f1
Isak Andersson <BitPuffin@lavabit.com>
2013-02-01 12:27:21 +0100

    Created the basic RestClient object structure with constructor, hopefully thats final

--- source/douchvibe/db.d
+++ source/douchvibe/db.d
@@ -14,13 +14,28 @@ class Database {
 class RestClient {
     private:
         HttpClient client;
+        string host;
+        ushort port;
+        string url_http_signature;
 
     public:
-        this(string host="127.0.0.1", ushort port = 5984u, bool ssl=false) {
+        this(string host = "127.0.0.1", ushort port = 5984u, bool ssl = false) {
             client = connectHttp(host, port, ssl);
+            client.disconnect();
+            
+            this.host = host;
+            this.port = port;
+            url_http_signature = (ssl ? "https" : "http") ~ "://";
         }
 
-        ~this() {
-            client.disconnect();
+        // Do a GET request at the specified path and return a Json object
+        Json get(string path) {
+
         }
+        
+        // Don't parse JSON
+        HttpResponse getRaw(string path) {
+            
+        }
+
 }
