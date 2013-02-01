255309c25e92f066621c3b45f7d417892892869d
Isak Andersson <BitPuffin@lavabit.com>
2013-02-01 12:55:07 +0100

    added a get request for raw data, not sure if it works yet

--- source/douchvibe/db.d
+++ source/douchvibe/db.d
@@ -2,6 +2,7 @@ module douchvibe.db;
 
 import vibe.http.client;
 import vibe.data.json;
+import vibe.inet.url;
 
 class Server {
     
@@ -17,6 +18,7 @@ class RestClient {
         string host;
         ushort port;
         string url_http_signature;
+        bool ssl;
 
     public:
         this(string host = "127.0.0.1", ushort port = 5984u, bool ssl = false) {
@@ -26,6 +28,7 @@ class RestClient {
             this.host = host;
             this.port = port;
             url_http_signature = (ssl ? "https" : "http") ~ "://";
+            this.ssl = ssl;
         }
 
         // Do a GET request at the specified path and return a Json object
@@ -33,9 +36,23 @@ class RestClient {
 
         }
         
-        // Don't parse JSON
-        HttpResponse getRaw(string path) {
-            
+        // Don't parse JSON just get the raw data
+        ubyte[] getRaw(string path) {
+            client.connect(url.host, url.port, ssl);
+            auto response = commonGet(path);
+            response.lockedConnection = client;
+            response.bodyReader = response.bodyReader; // Not sure why/if necessary, vibe did it though
+            ubyte[] data;
+            response.bodyReader.read(data);
+            return data;
         }
 
+    private:
+        auto commonGet(string path) {
+            Url url = Url.parse(url_http_signature ~ host ~ ":" ~ "/" ~ path);
+            return client.request( (req) {
+                req.requestUrl = url.localURI;
+                req.headers["Host"] = url.host;
+            });
+        }
 }
