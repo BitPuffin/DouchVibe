d492cc03b4c85ee7195b07fe62058bd0eebc5efb
Isak Andersson <BitPuffin@lavabit.com>
2013-02-24 02:26:47 +0100

    restapi is shaping up, now I need to clean it up and add error handling

--- source/app.d
+++ source/app.d
@@ -11,21 +11,5 @@ import vibe.d;
 import douchvibe.db;
 
 static this() {
-    auto db = Database.get("foo");  // Gets the foo db and initializes it if it doesn't exist, using default port and host
 
-    auto json_data = Json.EmptyObject;
-    json_data.name = "Bar";
-    json_data.age = 34;
-
-    auto doc = new Document(json_data);
-    db.put(doc);
-
-    auto doc_id = doc["_id"];
-    auto doc_rev = doc["_rev"];
-
-    // alternatively..
-    auto server = new Server(); // Gets a reference to the server using default port and host
-    auto db1 = new Database(server, "faa");
-    assert(db == db1);  // They are the same object
-    auto db2 = new Database(server, "bar");
 }
--- source/douchvibe/db.d
+++ source/douchvibe/db.d
@@ -1,10 +1,7 @@
 module douchvibe.db;
 
 import std.conv;
-
-import vibe.http.client;
-import vibe.data.json;
-import vibe.inet.url;
+import vibe.vibe;
 
 class Server {
     
@@ -16,66 +13,118 @@ class Database {
 class Document {
 }
 
-class RestClient {
+class RestAPI {
+    
+    alias StrMapCI Headers; // Because fuck that
+
     private:
         HttpClient client;
         string host;
         ushort port;
         string url_http_signature;
         bool ssl;
+        Headers headers;
 
     public:
-        this(string host = "127.0.0.1", ushort port = 5984u, bool ssl = false) {
-            client = connectHttp(host, port, ssl);
-            client.disconnect();
+        this(string host = "127.0.0.1", ushort port = 5984u, bool ssl = false, Headers headers = ["Content-Type" : "application/json"]) {
+            client = new HttpClient; // Only allocate a client once, efficiency++
             
             this.host = host;
             this.port = port;
             url_http_signature = (ssl ? "https" : "http") ~ "://";
             this.ssl = ssl;
+            this.headers = headers;
+        }
+
+        auto getResponse(string path, Headers headers = this.headers) {
+            return request(path, headers);
         }
 
         // Do a GET request at the specified path and return a Json object
-        Json get(string path) {
-            Url url = parsePath(path);
-            
-            client.connect(url.host, url.port, ssl);
-
-            auto response = commonGet(url);
-            auto json = response.readJson();
-
-            client.disconnect();
-
-            return json;
+        Json get(string path, Headers headers = this.headers) {
+            return request(path, headers).readJson();
         }
         
         // Don't parse JSON just get the raw data
-        ubyte[] getRaw(string path) {
-            Url url = parsePath(path);
-
-            client.connect(url.host, url.port, ssl);
-
-            auto response = commonGet(url);
+        ubyte[] getRaw(string path, Headers headers = this.headers) {
+            auto response = request(path, headers);
             response.lockedConnection = client;
             response.bodyReader = response.bodyReader; // Not sure why/if necessary, vibe did it though
 
             ubyte[] data;
             response.bodyReader.read(data);
 
-            client.disconnect();
-
             return data;
+        }
+
+        HttpResponse postResponse(string path, Json data, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.POST, cast(ubyte[])data.toString());
+        }
+
+        HttpResponse postResponse(string path, ubyte[] data, headers headers = this.headers) {
+            return request(path, headers, HttpMethod.POST, data);
+
+        Json post(string path, Json data, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.POST, cast(ubyte[])data.toString()).readJson();
+        }
+
+        Json post(string path, ubyte[] data, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.POST, data).readJson();
+        }
+
+        HttpResponse putResponse(string path, Json data, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.PUT, cast(ubyte[])data.toString());
+        }
+
+        HttpResponse putResponse(string path, ubyte[] data, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.PUT, data);
+        }
+
+        Json put(string path, Json data, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.PUT, cast(ubyte[])data.toString()).readJson();
+        }
+
+        Json put(string path, ubyte[] data, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.PUT, data).readJson;
+        }
+
+        HttpResponse deleteResponse(string path, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.DELETE);
+        }
+
+        Json delete(string path, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.DELETE).readJson();
+
+        HttpResponse copy(string path, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.COPY);
+        }
+
+        Json copy(string path, Headers headers = this.headers) {
+            return request(path, headers, HttpMethod.COPY).readJson();
+        }
+
+        auto request(string path, Headers headers = this.headers, HttpMethod meth /*dundudrugs*/ = HttpMethod.GET, ubyte[] data = null) {
+            Url url = parsePath(path);
+            
+            client.connect(url.host, url.port, ssl);
+
+            auto res = client.request( (req) {
+                req.requestUrl = url.localURI;
+                req.headers["Host"] = url.host;
+                req.headers ~= headers;
+                req.method = meth;
+
+                if (data) {
+                    req.writeBody(data, req.contentType);
+                }
+            });
+
+            client.disconnect();
+            return res;
         }
 
     private:
         auto parsePath(string path) {
             return Url.parse(url_http_signature ~ host ~ ":" ~ to!string(port) ~ "/" ~ path);
-        }
-
-        auto commonGet(Url url) {
-            return client.request( (req) {
-                req.requestUrl = url.localURI;
-                req.headers["Host"] = url.host;
-            });
         }
 }
